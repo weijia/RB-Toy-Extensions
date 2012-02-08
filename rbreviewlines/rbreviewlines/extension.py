7ddc042c4628f62bc5d6f37e9fb66a45dc21202e
Richard <richardwangwang@gmail.com>
2012-02-08 17:57:24 +0800

    Added extension.

---
+++ rbreviewlines/rbreviewlines/extension.py
@@ -0,0 +1,22 @@
+# Reports extension for Review Board.
+from django.conf import settings
+from django.conf.urls.defaults import patterns, include
+from reviewboard.extensions.base import Extension
+from reviewboard.extensions.hooks import DashboardHook, URLHook
+
+
+class RBReviewLinesDashboardHook(DashboardHook):
+    def get_entries(self):
+        return [{
+            'label': 'Review lines statistics',
+            'url': settings.SITE_ROOT + 'rbreviewlines/',
+        }]
+
+
+class RBReviewLinesExtension(Extension):
+    is_configurable = True
+    #requirements = ['extc.extension.ExtCExtension']
+
+    def __init__(self):
+        Extension.__init__(self)
+        self.dashboard_hook = RBReviewLinesDashboardHook(self)
---
+++ rbreviewlines/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+PACKAGE="RB-ReviewLines"
+VERSION="0.1"
+
+setup(
+    name=PACKAGE,
+    version=VERSION,
+    description="""Review lines extension for Review Board""",
+    author="Richard",
+    packages=["rbreviewlines"],
+    entry_points={
+        'reviewboard.extensions':
+        '%s = rbreviewlines.extension:RBReviewLinesExtension' % PACKAGE,
+    },
+    package_data={
+        'rbreviewlines': [
+            'htdocs/css/*.css',
+            'htdocs/js/*.js',
+            'templates/extd/*.html',
+            'templates/extd/*.txt',
+        ],
+    },
+    install_requires=['RB-Stats>=0.1']
+)
+
+
